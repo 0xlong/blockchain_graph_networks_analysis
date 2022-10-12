4a8cfdb5d2570f45160a4c3220b3b069e207babd
Longin_Imperial <longin.le18@imperial.ac.uk>
2022-10-12 13:53:50 +0200

    BGNA | Project set-up

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this project blockchain networks will be analyzed using specific methodologies from network science.',
+    author='0xlong',
+    license='MIT',
+)
